cb258e135f89b9448d006d04a2a65f0285762355
diez69 <davidtran69@hotmail.fr>
2017-12-14 14:51:13 +0100

    [ADD] Player class

---
+++ src/model/Player.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Player {
+    private String _name;
+
+    public Player(String name) {
+        _name = name;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+}
